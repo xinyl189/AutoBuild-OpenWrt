f1affaf75ae0767980ce0aa4ec3ea039431f7f95
xinyl189 <60832974+xinyl189@users.noreply.github.com>
2020-07-10 00:59:04 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,4 +7,4 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' openwrt/feeds.conf.default
